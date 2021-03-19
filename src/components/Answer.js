0fdcaaf8a3c3ea4942588c86ad0a0808e40bc68c
衞藤勝也 <masaya@etomasauyanoMacBook-Pro.local>
2021-03-20 01:27:29 +0900

    質問と回答を切り替えるメソッドを追加

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,17 +1,14 @@
 import React from "react"
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    
-  },
-}));
 
 const Answer = (props) => {
   //const classes = useStyles();
+  console.log("Answer",props)
   return(
-    <Button variant="contained">{props.content}</Button>
+    <Button variant="contained" onClick={() => props.select(props.content, props.nextId)}>
+      {props.content}
+    </Button>
   )
 }
 
